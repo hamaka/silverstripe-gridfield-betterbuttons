a3c41bdb18b8ce8500c904b4aeb61bbf12b44e32
Carlo <carlo@hamaka.nl>
2019-02-26 10:19:14 +0100

    Is versioned

--- src/Buttons/Delete.php
+++ src/Buttons/Delete.php
@@ -1,52 +1,56 @@
 <?php
-
-namespace UncleCheese\BetterButtons\Buttons;
-
-use SilverStripe\ORM\DataObject;
-use SilverStripe\Versioned\Versioned;
-use SilverStripe\View\Requirements;
-
-/**
- * Defines the button that deletes a record, with confirmation
- *
- * @author  Uncle Cheese <unclecheese@leftandmain.com>
- * @package  silverstripe-gridfield-betterbuttons
- */
-class Delete extends Button
-{
+    
+    namespace UncleCheese\BetterButtons\Buttons;
+    
+    use SilverStripe\ORM\DataObject;
+    use SilverStripe\Versioned\Versioned;
+    use SilverStripe\View\Requirements;
+    
     /**
-     * Builds the button
+     * Defines the button that deletes a record, with confirmation
+     *
+     * @author   Uncle Cheese <unclecheese@leftandmain.com>
+     * @package  silverstripe-gridfield-betterbuttons
      */
-    public function __construct()
+    class Delete extends Button
     {
-        parent::__construct('doDelete', _t('GridFieldDetailForm.Delete', 'Delete'));
+        /**
+         * Builds the button
+         */
+        public function __construct()
+        {
+            parent::__construct('doDelete', _t('GridFieldDetailForm.Delete', 'Delete'));
+        }
+        
+        /**
+         * Adds the JS, sets up necessary HTML attributes
+         * @return Delete
+         */
+        public function baseTransform()
+        {
+            parent::baseTransform();
+            Requirements::javascript('unclecheese/betterbuttons:javascript/gridfield_betterbuttons_delete.js');
+            
+            return $this
+              ->setUseButtonTag(true)
+              ->addExtraClass('btn-danger-outline btn-hide-outline font-icon-trash-bin gridfield-better-buttons-delete')
+              ->setAttribute("data-toggletext", _t('GridFieldBetterButtons.AREYOUSURE', 'Yes. Delete this item.'))
+              ->setAttribute("data-confirmtext", _t('GridFieldDetailForm.CANCELDELETE', 'No. Don\'t delete.'));
+        }
+        
+        /**
+         * Determines if the button should show
+         * @return boolean
+         */
+        public function shouldDisplay()
+        {
+            /* @var DataObject|Versioned $record */
+            $record = $this->getGridFieldRequest()->getRecord();
+            
+            if ( ! $record->hasExtension(Versioned::class)) {
+                return $record->canDelete();
+            }
+            
+            return ! $record->isPublished() && $record->canDelete();
+        }
     }
-
-    /**
-     * Adds the JS, sets up necessary HTML attributes
-     * @return Delete
-     */
-    public function baseTransform()
-    {
-        parent::baseTransform();
-        Requirements::javascript('unclecheese/betterbuttons:javascript/gridfield_betterbuttons_delete.js');
-
-        return $this
-            ->setUseButtonTag(true)
-            ->addExtraClass('btn-danger-outline btn-hide-outline font-icon-trash-bin gridfield-better-buttons-delete')
-            ->setAttribute("data-toggletext", _t('GridFieldBetterButtons.AREYOUSURE', 'Yes. Delete this item.'))
-            ->setAttribute("data-confirmtext", _t('GridFieldDetailForm.CANCELDELETE', 'No. Don\'t delete.'));
-    }
-
-    /**
-     * Determines if the button should show
-     * @return boolean
-     */
-    public function shouldDisplay()
-    {
-        /* @var DataObject|Versioned $record */
-        $record = $this->getGridFieldRequest()->getRecord();
-
-        return !$record->isPublished() && $record->canDelete();
-    }
-}
